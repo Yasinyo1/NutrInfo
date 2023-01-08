4d2720d14e26f7b864dde8cb51d833b4e249dc6e
Yasinyo1 <Yasinyo@live.com>
2023-01-08 15:02:30 +0100

    BarcodeScanning works 2.0

--- app/src/main/java/com/example/nutrinfo/MainActivity.java
+++ app/src/main/java/com/example/nutrinfo/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.nutrinfo;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.view.OrientationEventListener;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.core.content.ContextCompat;
 
+import com.budiyev.android.codescanner.AutoFocusMode;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
 import com.example.nutrinfo.databinding.ActivityMainBinding;
 import com.google.zxing.Result;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
 
     private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +41,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
         setContentView(R.layout.activity_main);
+
+
+        ImageCapture imageCapture =
+                new ImageCapture.Builder()
+                        .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                        .build();
+
+        OrientationEventListener orientationEventListener = new OrientationEventListener((Context) this) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                int rotation;
+
+                // Monitors orientation values to determine the target rotation value
+                if (orientation >= 45 && orientation < 135) {
+                    rotation = Surface.ROTATION_270;
+                } else if (orientation >= 135 && orientation < 225) {
+                    rotation = Surface.ROTATION_180;
+                } else if (orientation >= 225 && orientation < 315) {
+                    rotation = Surface.ROTATION_90;
+                } else {
+                    rotation = Surface.ROTATION_0;
+                }
+
+                imageCapture.setTargetRotation(rotation);
+            }
+        };
+        orientationEventListener.enable();
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(file).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                        // insert your code here.
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+
+                    }
+
+                }
+        );
+
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull final Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+
+        mCodeScanner.setAutoFocusMode(AutoFocusMode.CONTINUOUS);
+        mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
+        mCodeScanner.setDecodeCallback(new
+
+                                               DecodeCallback() {
+                                                   @Override
+                                                   public void onDecoded(@NonNull final Result result) {
+                                                       runOnUiThread(new Runnable() {
+                                                           @Override
+                                                           public void run() {
+                                                               Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                                                           }
+                                                       });
+                                                   }
+                                               });
         scannerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

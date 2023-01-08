ac9878693b3f7624aaa871985be466859318eb7c
cedbanana <a.munteanu@student.utwente.nl>
2023-01-08 23:01:27 +0100

    Added Scan button that will activate the camera with which you will take a picture that will get processed and analyzed with the tenserflow dataset

--- app/src/main/java/com/example/nutrinfo/MainActivity.java
+++ app/src/main/java/com/example/nutrinfo/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.nutrinfo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,18 +23,48 @@ import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.example.nutrinfo.databinding.ActivityMainBinding;
+import com.example.nutrinfo.ml.ModelUnquant;
 import com.google.zxing.Result;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 
 public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
-
+    ImageView imageView;
     private ActivityMainBinding binding;
+    int imageSize = 224 ;
+    TextView result;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
+        result  = findViewById(R.id.tv_textResult);
         super.onCreate(savedInstanceState);
-
+        ActivityResultLauncher<Intent> actvityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        int resultCode = result.getResultCode();
+                        Intent data =result.getData();
+                        if(resultCode==RESULT_OK){
+                            Bitmap photo = (Bitmap) data.getExtras().get("data");
+                            imageView.setImageBitmap(photo);
+                            photo = Bitmap.createScaledBitmap(photo,imageSize,imageSize,false);
+                            classifyImage(photo);
+                        } else {
+                            Toast.makeText(MainActivity.this,"Cancelled",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
@@ -47,6 +88,58 @@ public class MainActivity extends AppCompatActivity {
                 mCodeScanner.startPreview();
             }
         });
+    // SCAN BUTTON
+        Button ScanBtn = findViewById(R.id.ScanBtn);
+        ScanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                actvityResultLauncher.launch(intent);
+            }
+        });
+    }
+
+    public void classifyImage(Bitmap image){
+        try {
+            ModelUnquant model = ModelUnquant.newInstance(context);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4*imageSize*imageSize*3);
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int [] intValues = new int[imageSize*imageSize];
+            image.getPixels(intValues,0,image.getWidth(),0,0,image.getWidth(), image.getHeight());
+            int nr = 0; // the pixel
+            for(int i = 0 ; i<imageSize;i++){
+                for(int j = 0 ; j<imageSize;j++){
+                    int val = intValues[nr++];
+                    byteBuffer.putFloat(((val>>16)&0xFF)*(1.f/255.f));
+                    byteBuffer.putFloat(((val>>8)&0xFF)*(1.f/255.f));
+                    byteBuffer.putFloat((val&0xFF)*(1.f/255.f));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+            // Runs model inference and gets result.
+            ModelUnquant.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            int maxP = 0 ;
+            float maxConfidence=0;
+            for (int i =0 ;i<confidences.length;i++){
+                if(confidences[i]>maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxP=i;
+                }
+            }
+            String[] classes = {"apple","banana","orange"};
+            result.setText(classes[maxP]);
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
     }
 
     @Override
